fcde9a44c610228b2d491cc2a233166d0edf5afd
Shubhankar Seth <shubhseth@gmail.com>
2022-12-01 18:12:18 +0530

    part1, exercise 1

---
+++ part1/course-information/src/App.js
@@ -0,0 +1,58 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  const { part1, part2, part3 } = props.content
+  return (
+    <div>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        content={{
+          part1: { name: part1, exercises: exercises1 },
+          part2: { name: part2, exercises: exercises2 },
+          part3: { name: part3, exercises: exercises3 },
+        }}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
