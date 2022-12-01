d3a17062465965fcd0ba65ce07e583d76c9328df
Shubhankar Seth <shubhseth@gmail.com>
2022-12-01 18:17:30 +0530

    1.2: course information, step 2

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -6,19 +6,17 @@ const Header = (props) => {
   );
 };
 
+const Part = (props) => {
+ return <p>{props.name} {props.exercises}</p>
+};
+
 const Content = (props) => {
   const { part1, part2, part3 } = props.content
   return (
     <div>
-      <p>
-        {part1.name} {part1.exercises}
-      </p>
-      <p>
-        {part2.name} {part2.exercises}
-      </p>
-      <p>
-        {part3.name} {part3.exercises}
-      </p>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
